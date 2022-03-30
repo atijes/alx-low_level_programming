324cf3f77ad9835f6001f1981a65bddb65601ce0
atijes <jesmil_@outlook.com>
2022-03-30 12:50:46 +0000

    betty coding style correction in putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* Display of the alphabets in lowercase, 
+* followed by a new line
+*/
+
+void print_alphabet(void)
+{
+char ch;
+
+ch = 'a';
+
+while (ch <= 'z')
+
+{
+
+putchar(ch);
+
+ch++;
+
+}
+
+putchar('\n');
+
+}
