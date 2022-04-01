d895ca2b5ea4644c71fba765b49c173eb967220d
atijes <jesmil_@outlook.com>
2022-04-01 22:52:48 +0000

    A program that prints its name

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+/**
+ * This file is the header file 
+ * It contains all the function prototypes used in the files.
+ */
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
